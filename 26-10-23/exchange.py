7281bab0d9a7aafbda9fc0dcccad16421acaa1f8
selvaah <145668159+selvaah@users.noreply.github.com>
2023-10-27 15:15:05 +0530

    Delete 26-10-23 directory

--- 26-10-23/exchange.py
+++
@@ -1,3 +0,0 @@
-text=input("Enter a string:")
-newtext=text[-1]+text[1: -1]+text[0]
-print("New string:",newtext)
--- 26-10-23/sorted.py
+++
@@ -1,6 +0,0 @@
-d={1:5,7:7,3:9,5:11,0:13}
-print("THE DICTIONARY:",d)
-sorted_d=sorted(d.items())
-print("DICTIONARY IN ASCENDING:",sorted_d)
-sorted_rev=sorted(d.items(),reverse=True)
-print("DICTIONARY IN DESCENDing:",sorted_rev)
